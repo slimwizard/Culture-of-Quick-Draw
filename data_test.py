0d104f657f197af0fde5e2e6cb7fb461288a9a51
Matthew Rice <mrmatthew024@gmail.com>
2018-04-26 12:22:30 -0500

    Add files via upload

--- data_test.py
+++ data_test.py
@@ -3,8 +3,11 @@ import jsonlines as jl
 from pprint import pprint
 import itertools
 
-file_list = ['car', 'shoe', 'backpack', 'sun', 'power_outlet']
-for _file in file_list:
+#file_LIST = ['car', 'shoe', 'backpack', 'sun', 'power_outlet']
+file_LIST = ["bird", "birthdaycake"]
+
+
+for _file in file_LIST:
 
     NorthAmerica = ["AI", "AG", "AW", "BS", "BB", "BZ", "BM", "BQ", "VG", "CA", "KY", "CR", "CU", "CW", "DM", "DO", "SV", "GL", "GD", "GP", "GT", "HT", "HN", "JM", "MQ", "MX", "PM", "MS", "CW", "KN", "NI", "PA", "PR", "BQ", "SX", "LC", "PM", "VC", "TT", "TC", "US", "VI"]
     SouthAmerica = ["AR", "BO", "BR", "CL", "CO", "EC", "FK", "GF", "GY", "GY", "PY", "PE", "SR", "UY", "VE"]
@@ -13,81 +16,97 @@ for _file in file_list:
     Asia = ["AF", "AM", "AZ", "BH", "BD", "BT", "BN", "KH", "CN", "CX", "CC", "IO", "GE", "HK", "IN", "ID", "IR", "IQ", "IL", "JP", "JO", "KZ", "KW", "KG", "LA", "LB", "MO", "MY", "MV", "MN", "MM", "NP", "KP", "OM", "PK", "PS", "PH", "QA", "SA", "SG", "KR", "LK", "SY", "TW", "TJ", "TH", "TR", "TM", "AE", "UZ", "VN", "YE"]
     Oceana_Australia= ["AS", "AU", "NZ", "CK", "TL", "FM", "FJ", "PF", "GU", "KI", "MP", "MH", "UM", "NR", "NC", "NZ", "NU", "NF", "PW", "PG", "MP", "WS", "SB", "TK", "TO", "TV", "VU", "UM", "WF"]
 
-
-    overall_list = []
-    NA_list = []
-    SA_list = []
-    AF_list = []
-    EU_list = []
-    AS_list = []
-    OA_list = []
+    OVERALL_LIST = []
+    NA_LIST = []
+    SA_LIST = []
+    AF_LIST = []
+    EU_LIST = []
+    AS_LIST = []
+    OA_LIST = []
+    NON_NA_LIST = []
 
     with jl.open(f"./data/{_file}.ndjson") as reader:
-        index = 0
         for obj in reader:
             d={}
-            d['countrycode'] = obj['countrycode']
-            d['drawing'] = obj['drawing']
-            index += 1
-            overall_list.append(d)
+            d["continentcode"] = ""
+            d["drawing"] = obj["drawing"]
+            if obj["countrycode"] in NorthAmerica:
+                d["continentcode"] = "NA"
+                NA_LIST.append(d)
+            elif obj["countrycode"] in SouthAmerica:
+                d["continentcode"] = "SA"
+                SA_LIST.append(d)
+                d["continentcode"] = "NON_NA"
+                NON_NA_LIST.append(d)
+            elif obj["countrycode"] in Africa:
+                d["continentcode"] = "AF"
+                AF_LIST.append(d)
+                d["continentcode"] = "NON_NA"
+                NON_NA_LIST.append(d)
+            elif obj["countrycode"] in Europe:
+                d["continentcode"] = "EU"
+                EU_LIST.append(d)
+                d["continentcode"] = "NON_NA"
+                NON_NA_LIST.append(d)
+            elif obj["countrycode"] in Asia:
+                d["continentcode"] = "AS"
+                AS_LIST.append(d)
+                d["continentcode"] = "NON_NA"
+                NON_NA_LIST.append(d)
+            elif obj["countrycode"] in Oceana_Australia:
+                d["continentcode"] = "OA"
+                OA_LIST.append(d)
+                d["continentcode"] = "NON_NA"
+                NON_NA_LIST.append(d)
 
-    for i in overall_list:
-        if i['countrycode'] in NorthAmerica:
-            NA_list.append(i)
-        elif i['countrycode'] in SouthAmerica:
-            SA_list.append(i)
-        elif i['countrycode'] in Africa:
-            AF_list.append(i)
-        elif i['countrycode'] in Europe:
-            EU_list.append(i)
-        elif i['countrycode'] in Asia:
-            AS_list.append(i)
-        elif i['countrycode'] in Oceana_Australia:
-            OA_list.append(i)
-    file = open(f'./data/NA_{_file}.jsonl', 'w')
-    for json in NA_list:
-        json['countrycode'] = 'NA'
+    file = open(f'./data/{_file}/1/NA_{_file}.jsonl', 'w')
+    for json in NA_LIST:
         file.write(f'{json}\n')
     file.close()
-    file = open(f'./data/SA_{_file}.jsonl', 'w')
-    for json in SA_list:
-        json['countrycode'] = 'SA'
+    file = open(f'./data/{_file}/2/NA_{_file}.jsonl', 'w')
+    for json in NA_LIST:
         file.write(f'{json}\n')
     file.close()
-    file = open(f'./data/AF_{_file}.jsonl', 'w')
-    for json in AF_list:
-        json['countrycode'] = 'AF'
+    file = open(f'./data/{_file}/1/SA_{_file}.jsonl', 'w')
+    for json in SA_LIST:
         file.write(f'{json}\n')
     file.close()
-    file = open(f'./data/EU_{_file}.jsonl', 'w')
-    for json in EU_list:
-        json['countrycode'] = 'EU'
+    file = open(f'./data/{_file}/1/AF_{_file}.jsonl', 'w')
+    for json in AF_LIST:
         file.write(f'{json}\n')
     file.close()
-    file = open(f'./data/AS_{_file}.jsonl', 'w')
-    for json in AS_list:
-        json['countrycode'] = 'AS'
+    file = open(f'./data/{_file}/1/EU_{_file}.jsonl', 'w')
+    for json in EU_LIST:
         file.write(f'{json}\n')
     file.close()
-    file = open(f'./data/OA_{_file}.jsonl', 'w')
-    for json in OA_list:
-        json['countrycode'] = 'OA'
+    file = open(f'./data/{_file}/1/AS_{_file}.jsonl', 'w')
+    for json in AS_LIST:
         file.write(f'{json}\n')
     file.close()
+    file = open(f'./data/{_file}/1/OA_{_file}.jsonl', 'w')
+    for json in OA_LIST:
+        file.write(f'{json}\n')
+    file.close()
+    file = open(f'./data/{_file}/2/NON_NA_{_file}.jsonl', 'w')
+    for json in NON_NA_LIST:
+        file.write(f"{json}\n")
+    file.close()
 
-    print(f"Total {_file} doodles: " + str(len(overall_list)) + "\n")
+    print(f"Total {_file} doodles: " + str(len(OVERALL_LIST)) + "\n")
 
-    print("North America total: \n" + str(len(NA_list)) + "\n")
+    print("North America total: \n" + str(len(NA_LIST)) + "\n")
 
-    print("South American total: " + str(len(SA_list)) + "\n")
+    print("South American total: " + str(len(SA_LIST)) + "\n")
 
-    print("Africa total: " + str(len(AF_list)) + "\n")
+    print("Africa total: " + str(len(AF_LIST)) + "\n")
 
-    print("Europe total: \n" + str(len(EU_list)) + "\n")
+    print("Europe total: \n" + str(len(EU_LIST)) + "\n")
 
-    print("Asia total: " + str(len(AS_list)) + "\n")
+    print("Asia total: " + str(len(AS_LIST)) + "\n")
 
-    print("Oceana/Australia total: " + str(len(OA_list)) + "\n")
+    print("Oceana/Australia total: " + str(len(OA_LIST)) + "\n")
+
+    print("Non North America total: " + str(len(NON_NA_LIST)) + "\n")
 
     print("-"*20)
 
