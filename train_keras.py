e7ff5fcf2b24bd3fff5ea61237d69986bd653f76
Grant <g.free697@gmail.com>
2018-04-30 19:15:31 -0500

    keras models

---
+++ train_keras.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.metrics import accuracy_score, confusion_matrix, roc_curve, roc_auc_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+
+EU_car = np.load('new_data/car/EU_car.npy')
+AS_car = np.load('new_data/car/AS_car.npy')
+AF_car = np.load('new_data/car/AF_car.npy')
+NA_car = np.load('new_data/car/NA_car.npy')
+OA_car = np.load('new_data/car/OA_car.npy')
+SA_car = np.load('new_data/car/SA_car.npy')
+Non_NA_car = np.load('new_data/car/NON_NA_car.npy')
+
+NA_car = np.c_[NA_car, np.zeros(len(NA_car))]
+Non_NA_car = np.c_[Non_NA_car, np.ones(len(Non_NA_car))]
+
+
+# merge the cat and sheep arrays, and split the features (X) and labels (y). Convert to float32 to save some memory.
+X = np.concatenate((NA_car[:20000,:-1], Non_NA_car[:20000,:-1]), axis=0).astype('float32') # all columns but the last
+y = np.concatenate((NA_car[:20000,-1], Non_NA_car[:20000,-1]), axis=0).astype('float32') # the last column
+
+# train/test split (divide by 255 to obtain normalized values between 0 and 1)
+# I will use a 50:50 split, since I want to start by training the models on 5'000 samples and thus have plenty of samples to spare for testing.
+X_train, X_test, y_train, y_test = train_test_split(X/255.,y,test_size=0.5,random_state=0)
+
+# one hot encode outputs
+y_train_cnn = np_utils.to_categorical(y_train)
+y_test_cnn = np_utils.to_categorical(y_test)
+num_classes = y_test_cnn.shape[1]
+
+
+# reshape to be [samples][pixels][width][height]
+X_train_cnn = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+X_test_cnn = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+# define the CNN model
+def cnn_model():
+    # create model
+    model = Sequential()
+    model.add(Conv2D(30, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(15, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+#%%time
+
+np.random.seed(0)
+# build the model
+model_cnn = cnn_model()
+# Fit the model
+model_cnn.fit(X_train_cnn, y_train_cnn, validation_data=(X_test_cnn, y_test_cnn), epochs=10, batch_size=200)
+# Final evaluation of the model
+scores = model_cnn.evaluate(X_test_cnn, y_test_cnn, verbose=0)
+print('Final CNN accuracy: ', scores[1])
+
+# function to plot the 28x28 pixel images from numpy arrays
+
+def plot_samples(input_array, rows=4, cols=5, title=''):
+    '''
+    Function to plot 28x28 pixel drawings that are stored in a numpy array.
+    Specify how many rows and cols of pictures to display (default 4x5).
+    If the array contains less images than subplots selected, surplus subplots remain empty.
+    '''
+
+    fig, ax = plt.subplots(figsize=(cols,rows))
+    ax.axis('off')
+    plt.title(title)
+
+    for i in list(range(0, min(len(input_array),(rows*cols)) )):
+        a = fig.add_subplot(rows,cols,i+1)
+        imgplot = plt.imshow(input_array[i,:784].reshape((28,28)), cmap='gray_r', interpolation='nearest')
+        plt.xticks([])
+        plt.yticks([])
+
+    plt.show()
+
+
+model_cnn.save('models/car_model.h5')
+#plot_samples(NA_car)
+#plot_samples(Non_NA_car)
